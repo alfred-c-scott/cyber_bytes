478f669aef719757fc0220dcf58c28c15ef8693b
mick <alfred.scott@protonmail.com>
2023-04-26 20:05:19 -0400

    added app_name for url tag

--- hack_the_box/urls.py
+++ hack_the_box/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import HackTheBoxHomeView
 
+app_name = 'hack_the_box'
 urlpatterns = [
     path('', HackTheBoxHomeView.as_view(), name='hack_the_box_home')
     ]
--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import HomePageView, AboutPageView, ContactPageView, ResumePageView
 
+app_name = 'home'
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
