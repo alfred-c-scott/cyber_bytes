1feb09f526b5f7e12a2313241bf5ac53fe3f5961
mick <alfred.scott@protonmail.com>
2023-04-26 20:31:50 -0400

    removed 'home/' url path

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('contact/', ContactPageView.as_view(), name='contact'),
-    path('home/', HomePageView.as_view(), name='home'),
     path('resume/', ResumePageView.as_view(), name='resume'),
     ]
